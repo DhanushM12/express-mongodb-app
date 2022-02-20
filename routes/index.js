55560083de6a3e41256a635a006a8a17a6251cd7
DhanushM12 <dhanushmukhi@gmail.com>
2022-02-20 18:54:41 +0530

    updated get by id, delete and update

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,51 @@ router.get('/', async (req, res) => {
 })
 
 // get by id
+router.get('/:id', getSubscriber, async (req, res) => {
+    res.json(res.subscriber);
+})
+
 // delete the subscriber
+router.delete('/:id', getSubscriber, async (req, res) => {
+    try {
+        await res.subscriber.remove();
+        res.json({message: 'Deleted Subscriber'})
+    } catch (error) {
+        res.status(500).json({message: err.message});
+    }
+})
+
 // update the subscriber
+router.patch('/:id', getSubscriber, async (req, res) => {
+    if(req.body.name != null){
+        res.subscriber.name = req.body.name;
+    }
+    if(req.body.channel != null){
+        res.subscriber.channel = req.body.channel;
+    }
+    try {
+        const updatedSubscriber = await res.subscriber.save();
+        res.status(200).json(updatedSubscriber);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+
+// middleware to get the subscriber by id
+async function getSubscriber(req, res, next){
+    let subscriber;
+    try {
+        subscriber = await Subscriber.findById(req.params.id);
+        if(subscriber == null){
+            res.status(404).json({message: 'Cannot find the subscriber'});
+        }
+    } catch (error) {
+        return res.status(500).json({message: err.message});
+    }
+    res.subscriber = subscriber;
+    next();
+}
+
 
 module.exports = router;
